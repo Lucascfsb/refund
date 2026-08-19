828a00c305454fc63e8a4bccd9fb067257832af8
Lucas Castro <lucascfsb@gmail.com>
2026-08-18 22:58:39 -0300

    feat: add static file serving for uploads directory

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import cors from "cors";
 
 import { router } from "@/routes/index";
 import { errorHandling } from "@/middlewares/error-handling";
+import uploadConfig from "@/configs/upload";
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(router);
 
