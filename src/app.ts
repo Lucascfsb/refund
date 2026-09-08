e6a7cd6639158a774c03676fb4581c86c1b18890
Lucas Castro <lucascfsb@gmail.com>
2026-09-07 21:24:36 -0300

    feat: update package.json and package-lock.json with new dependencies and scripts; ensure uploads directory exists in app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
+import fs from "node:fs";
 
 import { router } from "@/routes/index";
 import { errorHandling } from "@/middlewares/error-handling";
 import uploadConfig from "@/configs/upload";
 
 const app = express();
+
+if (!fs.existsSync(uploadConfig.UPLOADS_FOLDER)) {
+  fs.mkdirSync(uploadConfig.UPLOADS_FOLDER, { recursive: true });
+}
 
 app.use(cors());
 
